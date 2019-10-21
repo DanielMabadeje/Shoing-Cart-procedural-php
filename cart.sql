c088ad6a1c9f7ad084b56a5706d89b59d98df790
mabadejedaniel1 <mabadejedaniel1@gmail.com>
2019-10-20 21:11:15 -0700

    added files

---
+++ cart.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `tbl_product` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `image` varchar(255) NOT NULL,
+  `price` double(10,2) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
